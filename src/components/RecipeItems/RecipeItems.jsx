3d9b2978ed7a69e5bf79bc240aa62b4e3d277b3b
Noah Greensweig <ngreensweig@gmail.com>
2023-12-29 11:39:45 -0600

    Set up recipe cards for /recipes view

--- src/components/RecipeItems/RecipeItems.jsx
+++ src/components/RecipeItems/RecipeItems.jsx
@@ -2,27 +2,60 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import './RecipeItems.css';
 
+import { Grid, Paper, Card, CardContent, CardMedia, CardActionArea, CardActions, Typography } from "@mui/material";
+
 function RecipeItems() {
 
     const dispatch = useDispatch();
 
     // Get recipes from Redux store
     const recipes = useSelector(store => store.recipeReducer);
+    // const firstPhoto = recipes[0].photo;
+    // console.log(firstPhoto);
 
     useEffect(() => {
         dispatch({ type: 'FETCH_RECIPES' });
     }, []);
 
     return (
+
         <>
-            <ul>
+        {/* <h1 color="black">{firstPhoto}</h1> */}
+            <Grid container spacing={2} alignItems={'center'} justifyContent={'center'} minHeight={'5vh'}>
+
                 {recipes.map(recipe => (
-                    <li key={recipe.id} style={{color: 'black'}}><img src={`images/${recipe.photo}`} width={100} height={100} /> 
-                    {recipe.title}
-                    </li>
+                    <Grid item xs={10} md={6}>
+                        <Paper elevation={5}>
+                            <Card>
+                                <CardMedia
+                                component={'img'}
+                                height={'194'}
+                                image={`images/${recipe.photo}`}
+                                alt={`${recipe.title} dish`}
+                            />
+                                <Typography style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    height: '100%',
+                                    fontFamily: 'inter',
+                                    color: 'black'
+                                }}
+                                    variant="h4"
+                                    component="div"
+                                    sx={{
+                                        fontWeight: 'bold',
+                                        mb: 2
+                                    }}>{recipe.title}</Typography>
+                            </Card>
+                        </Paper>
+                    </Grid>
                 ))}
-            </ul>
+
+            </Grid>
         </>
+
     )
 
 }
