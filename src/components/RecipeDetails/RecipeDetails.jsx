3c4178937fcff0524ddfa3471b9a6f85273cb180
Noah Greensweig <ngreensweig@gmail.com>
2024-01-08 09:58:59 -0600

    Set up parsing of recipe text into title, ingredients, and instructions into bullet points

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -28,9 +28,12 @@ function RecipeDetails() {
 
     const image = details ? details.photo : '';
     const instructions = details ? details.instructions : '';
-    const ingredients = instructions ? instructions.split('Instructions: ')[0].replace(/[,.]/g, '').split('-') : '';
-    console.log(typeof ingredients[0]);
-    // console.log(ingredients.map(ingredient => ingredient.length > 2 ? ingredient.replace(' \n', '').trim() : ''));
+    const ingredients = instructions ? instructions.split('Instructions:')[0].replace(/[,.]/g, '').split('-') : '';
+
+    console.log(instructions ? ingredients[8].split('Instructions:')[0] : null);
+    // console.log(instructions ? includesInstructions : null);
+
+    const recipeSteps = instructions ? instructions.split('Instructions:')[1].split(/\d+\./).filter(step => step.trim() !== '') : '';
 
     const saveEditedTitle = (e, id) => {
         e.preventDefault();
@@ -113,10 +116,15 @@ function RecipeDetails() {
                 style={{ borderRadius: '75%' }}
             />
             <p style={{ color: 'black' }}>Ingredients:</p>
-            {/* <ul>
-                {ingredients.map(ingredient => ingredient.length > 2 ? <li style={{ color: "black" }}>{ingredient.replace(' \n', '').trim()}</li> : '')}
-            </ul> */}
-            <p style={{ color: 'black' }}>Ingredients: {instructions}</p>
+            <ul>
+                {instructions ? ingredients.map(ingredient => ingredient.length > 2 ? <li style={{ color: "black" }}>{ingredient.replace(' \n', '').trim()}</li> : '') : null}
+            </ul>
+
+            <p style={{ color: 'black' }}>Instructions:</p>
+            <ol>
+                {instructions ? recipeSteps.map(step => step.length > 2 ? <li style={{ color: "black" }}>{step}</li> : '') : null}
+            </ol>
+
             {comments.map(comment => <p style={{ color: 'black' }}>{comment.comment}</p>)}
 
             <div style={{ display: 'flex' }}>
