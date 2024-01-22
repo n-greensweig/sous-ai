4f007d8c3b30b73919a1ae7af140efc6d3cbe290
Noah Greensweig <ngreensweig@gmail.com>
2024-01-22 11:18:31 -0600

    Deleted unnecesary EditPage directory

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -11,8 +11,6 @@ let pool;
 // When our app is deployed to the internet 
 // we'll use the DATABASE_URL environment variable
 // to set the connection info: web address, username/password, db name
-// eg: 
-//  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
 if (process.env.DATABASE_URL) {
     pool = new pg.Pool({
         connectionString: process.env.DATABASE_URL,
