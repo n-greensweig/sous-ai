b2b0c403b9b1082d4b096d67d72b726974986dc8
Noah Greensweig <ngreensweig@gmail.com>
2024-03-19 12:50:11 -0500

    Add logout icon in navbar.

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
   return (
-      <button
-        // This button shows up in multiple locations and is styled differently
-        // because it's styled differently depending on where it is used, the className
-        // is passed to it from it's parents through React props
-        className={props.className}
-        onClick={() => dispatch({ type: 'LOGOUT' })}
-      >
-        Log Out
-      </button>
+    <button
+      // This button shows up in multiple locations and is styled differently
+      // because it's styled differently depending on where it is used, the className
+      // is passed to it from it's parents through React props
+      className={props.className}
+      onClick={() => dispatch({ type: 'LOGOUT' })}
+    >
+      <LogoutIcon className='icon' />
+      Log Out
+    </button>
   );
 }
 
