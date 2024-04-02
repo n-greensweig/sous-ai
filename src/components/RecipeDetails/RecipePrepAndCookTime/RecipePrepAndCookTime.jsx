d264a1ea64db7574367d11b6a1146d36c38c3260
Noah Greensweig <ngreensweig@gmail.com>
2024-04-02 10:53:00 -0600

    Moved recipe prep and cook time styles into separate CSS file.

--- src/components/RecipeDetails/RecipePrepAndCookTime/RecipePrepAndCookTime.jsx
+++ src/components/RecipeDetails/RecipePrepAndCookTime/RecipePrepAndCookTime.jsx
@@ -1,18 +1,25 @@
-function RecipePrepAndCookTime(props) {
-    const prepTime = props.prepTime;
-    const cookTime = props.cookTime;
-    const replaceWithCommas = props.replaceWithCommas;
-    return (
-        prepTime ?
-            <p style={{ color: 'black', marginBottom: '0px', fontSize: '.9rem' }}>
-                <strong style={{ marginRight: '5px' }}>Prep Time</strong>
-                {prepTime ? replaceWithCommas(prepTime) : ''}
-            </p> : 
-            <p style={{ color: 'black', marginTop: '0px', fontSize: '.9rem' }}>
-                <strong style={{ marginRight: '5px' }}>Cook Time</strong>
-                {cookTime ? replaceWithCommas(cookTime) : ''}
-            </p>
-    )
+import "./RecipePrepAndCookTime.css";
 
+function RecipePrepAndCookTime({ prepTime, cookTime, replaceWithCommas }) {
+    return (
+        <div className="recipe-time">
+            {prepTime && (
+                <p className="recipe-time__prep">
+                    Prep Time
+                    <span className="recipe-time__value">{replaceWithCommas(prepTime)}</span>
+                </p>
+            )}
+            {cookTime && (
+                <p className="recipe-time__cook">
+                    Cook Time
+                    <span className="recipe-time__value">{replaceWithCommas(cookTime)}</span>
+                </p>
+            )}
+            {!prepTime && !cookTime && (
+                <p className="recipe-time">No prep or cook time available.</p>
+            )}
+        </div>
+    );
 }
+
 export default RecipePrepAndCookTime;
