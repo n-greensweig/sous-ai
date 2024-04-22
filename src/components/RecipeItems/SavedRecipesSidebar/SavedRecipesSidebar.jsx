09077a883ec7d74ee1567236014e2d82de02b836
Noah Greensweig <ngreensweig@gmail.com>
2024-04-22 11:29:11 -0500

    Got mobile navbar semi-functional.

--- src/components/RecipeItems/SavedRecipesSidebar/SavedRecipesSidebar.jsx
+++ src/components/RecipeItems/SavedRecipesSidebar/SavedRecipesSidebar.jsx
@@ -7,6 +7,7 @@ import './SavedRecipesSidebar.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
+
 // Importing Dialog components from Material UI for modal dialog functionality.
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -14,7 +15,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 // Importing Button and TextField components from Material UI for UI elements.
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, useTheme, useMediaQuery } from '@mui/material';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function SavedRecipesSidebar() {
@@ -43,87 +44,93 @@ function SavedRecipesSidebar() {
     // Toggles the isCreating state, controlling the visibility of the creation dialog.
     const toggleCreating = () => setIsCreating(!isCreating);
 
-    return (
-        <div className='sidebar__container'>
-            <div className="sidebar-content">
-                <p onClick={() => history.push('/recipe-box')} style={{
-                    backgroundColor:
-                        document.title === 'Saved Recipes' ? '#F8F8F5' : 'inherit',
-                    fontWeight: document.title === 'Saved Recipes' ? 'bold' : 'normal'
-                }}><BookmarkIcon className='sidebar__icon' /> Saved Recipes</p>
-                <p onClick={() => history.push('/recipe-box/cooked')}
-                    style={{
-                        backgroundColor:
-                            document.title === 'Cooked Recipes' ? '#F8F8F5' : 'inherit',
-                        fontWeight: document.title === 'Cooked Recipes' ? 'bold' : 'normal'
-                    }}
-                ><CheckCircleIcon className='sidebar__icon' /> Cooked Recipes</p>
-                <p
-                    onClick={() => history.push('/recipe-box/recent')}
-                    style={{
-                        backgroundColor:
-                            document.title === 'Recently Viewed Recipes' ? '#F8F8F5' : 'inherit',
-                        fontWeight: document.title === 'Recently Viewed Recipes' ? 'bold' : 'normal'
-                    }}
-                ><AccessTimeIcon className='sidebar__icon' /> Recently Viewed</p>
-                <p style={{
-                    backgroundColor:
-                        document.title === 'Grocery List' ? '#F8F8F5' : 'inherit',
-                    fontWeight: document.title === 'Grocery List' ? 'bold' : 'normal'
-                }}><ListAltIcon className='sidebar__icon' /> Grocery List</p>
-                <span className='sidebar__span--your-folders'>Your folders</span>
-                <div onClick={toggleCreating} className='div__icon__p--new-folder'>
-                    <Button className='icon--gray-border'
-                    ><AddIcon className='sidebar__icon sidebar__icon--add' /></Button>
-                    <p className='p__new-folder'>New Folder</p>
-                </div>
-                {recipeLists && recipeLists.map((list, index) => (
-                    <p onClick={() => {
-                        document.title = `Your Recipe Box - ${list.list_name}`;
-                        history.push(`/recipe-box/${list.id}`);
-                    }}
-                        style={{
-                            backgroundColor: document.title === `Your Recipe Box - ${list.list_name}` ? '#F8F8F5' : 'inherit',
-                        }}
-                    ><button key={index} style={{ color: 'black', cursor: 'pointer', fontWeight: document.title === `Your Recipe Box - ${list.list_name}` ? 'bold' : 'normal' }}>{list.list_name}</button></p>
+    // MUI theme hooks for responsive design
+    const theme = useTheme();
+    const isXsScreen = useMediaQuery(theme.breakpoints.down('xs'));
+    const isSmScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
-                ))}
-            </div>
-            {/* Dialog for creating a new recipe folder. */}
-            <Dialog open={isCreating}
-                onClose={toggleCreating}
-                PaperProps={{
-                    component: 'form',
-                    onSubmit: (event) => {
-                        event.preventDefault();
-                        saveRecipeList(listName);
-                        toggleCreating(event);
-                    },
-                }}>
-                <DialogTitle>Create new recipe folder</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>New recipe folder</DialogContentText>
-                    <TextField autoFocus
-                        margin="dense"
-                        id="title"
-                        name="title"
-                        fullWidth
-                        variant="standard"
-                        defaultValue={listName}
-                        onChange={e => setListName(e.target.value)}
-                        style={{ padding: '1px' }} />
-                </DialogContent>
-                <DialogActions>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
-                        <div className="first-row" style={{ width: '100%', marginBottom: '20px' }}>
-                            {/* Buttons for cancelling or saving the new recipe folder. */}
-                            <Button style={{ width: '50%', color: 'gray' }} onClick={() => setIsCreating(false)}>Cancel</Button>
-                            <Button variant="outlined" type="submit" style={{ width: '50%', color: '#DAA520', borderColor: '#DAA520' }}>Save</Button>
-                        </div>
+    return (
+        isXsScreen || isSmScreen ? null :
+            <div className='sidebar__container'>
+                <div className="sidebar-content">
+                    <p onClick={() => history.push('/recipe-box')} style={{
+                        backgroundColor:
+                            document.title === 'Saved Recipes' ? '#F8F8F5' : 'inherit',
+                        fontWeight: document.title === 'Saved Recipes' ? 'bold' : 'normal'
+                    }}><BookmarkIcon className='sidebar__icon' /> Saved Recipes</p>
+                    <p onClick={() => history.push('/recipe-box/cooked')}
+                        style={{
+                            backgroundColor:
+                                document.title === 'Cooked Recipes' ? '#F8F8F5' : 'inherit',
+                            fontWeight: document.title === 'Cooked Recipes' ? 'bold' : 'normal'
+                        }}
+                    ><CheckCircleIcon className='sidebar__icon' /> Cooked Recipes</p>
+                    <p
+                        onClick={() => history.push('/recipe-box/recent')}
+                        style={{
+                            backgroundColor:
+                                document.title === 'Recently Viewed Recipes' ? '#F8F8F5' : 'inherit',
+                            fontWeight: document.title === 'Recently Viewed Recipes' ? 'bold' : 'normal'
+                        }}
+                    ><AccessTimeIcon className='sidebar__icon' /> Recently Viewed</p>
+                    <p style={{
+                        backgroundColor:
+                            document.title === 'Grocery List' ? '#F8F8F5' : 'inherit',
+                        fontWeight: document.title === 'Grocery List' ? 'bold' : 'normal'
+                    }}><ListAltIcon className='sidebar__icon' /> Grocery List</p>
+                    <span className='sidebar__span--your-folders'>Your folders</span>
+                    <div onClick={toggleCreating} className='div__icon__p--new-folder'>
+                        <Button className='icon--gray-border'
+                        ><AddIcon className='sidebar__icon sidebar__icon--add' /></Button>
+                        <p className='p__new-folder'>New Folder</p>
                     </div>
-                </DialogActions>
-            </Dialog>
-        </div>
+                    {recipeLists && recipeLists.map((list, index) => (
+                        <p onClick={() => {
+                            document.title = `Your Recipe Box - ${list.list_name}`;
+                            history.push(`/recipe-box/${list.id}`);
+                        }}
+                            style={{
+                                backgroundColor: document.title === `Your Recipe Box - ${list.list_name}` ? '#F8F8F5' : 'inherit',
+                            }}
+                        ><button key={index} style={{ color: 'black', cursor: 'pointer', fontWeight: document.title === `Your Recipe Box - ${list.list_name}` ? 'bold' : 'normal' }}>{list.list_name}</button></p>
+
+                    ))}
+                </div>
+                {/* Dialog for creating a new recipe folder. */}
+                <Dialog open={isCreating}
+                    onClose={toggleCreating}
+                    PaperProps={{
+                        component: 'form',
+                        onSubmit: (event) => {
+                            event.preventDefault();
+                            saveRecipeList(listName);
+                            toggleCreating(event);
+                        },
+                    }}>
+                    <DialogTitle>Create new recipe folder</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>New recipe folder</DialogContentText>
+                        <TextField autoFocus
+                            margin="dense"
+                            id="title"
+                            name="title"
+                            fullWidth
+                            variant="standard"
+                            defaultValue={listName}
+                            onChange={e => setListName(e.target.value)}
+                            style={{ padding: '1px' }} />
+                    </DialogContent>
+                    <DialogActions>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+                            <div className="first-row" style={{ width: '100%', marginBottom: '20px' }}>
+                                {/* Buttons for cancelling or saving the new recipe folder. */}
+                                <Button style={{ width: '50%', color: 'gray' }} onClick={() => setIsCreating(false)}>Cancel</Button>
+                                <Button variant="outlined" type="submit" style={{ width: '50%', color: '#DAA520', borderColor: '#DAA520' }}>Save</Button>
+                            </div>
+                        </div>
+                    </DialogActions>
+                </Dialog>
+            </div>
     )
 }
 
