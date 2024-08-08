95fed443bb40f2714a41ec761dbe18acc6a4ed35
Noah Greensweig <ngreensweig@gmail.com>
2024-08-08 14:42:46 -0500

    Moved inline styling into RecipeCard.css from RecipeCard.jsx.

--- src/components/RecipeItems/RecipeGrid/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeItems/RecipeGrid/RecipeCard/RecipeCard.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from 'react';
 
 // Imports from Material-UI for UI components with responsive capabilities.
-import { Card, CardContent, CardMedia, CardActions, Typography, useTheme, useMediaQuery, Popover } from "@mui/material";
+import { Card, CardContent, CardMedia, CardActions, Typography, Popover } from "@mui/material";
 // useHistory hook from React Router for programmatically navigating to different routes.
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
@@ -32,10 +32,7 @@ function FadeIn({ children }) {
     });
 
     return (
-        <div
-            ref={ref}
-            style={{ opacity: inView ? 1 : 0, transition: 'opacity 0.5s ease-in' }}
-        >
+        <div ref={ref} className={inView ? 'in-view' : 'hidden'}>
             {children}
         </div>
     );
@@ -137,11 +134,6 @@ function RecipeCard(props) {
         }
     };
 
-    // Use Material-UI hooks to check for screen size for responsive layout design.
-    const theme = useTheme();
-    const isXsScreen = useMediaQuery(theme.breakpoints.down('xs'));
-    const isSmScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
     const handleDragStart = (e, target) => {
         dispatch({ type: 'FETCH_DETAILS', payload: props.recipe.id });
         e.dataTransfer.setData('recipeId', props.recipe.id);
@@ -161,86 +153,31 @@ function RecipeCard(props) {
         return (
             <FadeIn>
                 <Card
-                    className="recipe-card-hover" style={{ cursor: 'pointer', }}
-                    sx={{
-                        borderRadius: '1px', boxShadow: 'none', border: '1px solid #e6e6e6', width: isXsScreen || isSmScreen ? '160px' : '200px',
-                        height: isXsScreen || isSmScreen ? '247px' : null,
-                        '&:hover': {
-                            boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.2)',
-                        },
-                        '&:active': {
-                            cursor: 'pointer',
-                        },
-                    }}
+                    className="recipe-grid__card--container"
                     draggable
                     onDragStart={e => handleDragStart(e, props.recipe.id)}
                 >
                     <div key={props.recipe.id}>
                         <div onClick={() => handleClick(props.recipe.id)}>
-                            <CardMedia
-                                component={'img'}
-                                height={'130px'}
-                                image={`${props.recipe.display_photo}`}
-                                alt={`${props.recipe.title} dish`}
-                            />
-                            <CardContent className="card-content" style={{
-                                display: 'flex', flexDirection: 'column',
-                                justifyContent: 'flex-start',
-                                height: '30px', paddingLeft: '10px', paddingRight: '10px',
-                                overflow: 'clip'
-                            }}>
-                                {/* Typography for recipe title with responsive font size. */}
-                                <Typography className="title" style={{
-                                    fontFamily: 'inter',
-                                    color: 'black',
-                                    fontSize: '18px',
-                                    margin: '0px', marginLeft: '0px',
-                                }}
-                                    variant="h4"
-                                    component="div"
-                                    sx={{
-                                        fontWeight: 'bold',
-                                        mb: 2
-                                    }}>{props.recipe.title}</Typography>
+                            <CardMedia className="recipe-grid__card--media" image={`${props.recipe.display_photo}`} alt={`${props.recipe.title} dish`} />
+                            <CardContent className="recipe-grid__card--content">
+                                <Typography className="recipe-grid__card--recipe-title" variant="h4">{props.recipe.title}</Typography>
                             </CardContent>
                         </div>
-                        <div style={{
-                            display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',
-                            width: '100%', paddingLeft: '5px', margin: '0px',
-                        }}>
-                            <div onClick={() => handleClick(props.recipe.id)} style={{
-                                display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',
-                                width: '100%', paddingLeft: '5px', margin: '0px',
-                            }}>
-                                <Typography className="notes" style={{
-                                    alignItems: 'baseline',
-                                    justifyContent: 'center',
-                                    fontFamily: 'inter',
-                                    color: 'black',
-                                    fontSize: isXsScreen || isSmScreen ? '16px' : '13px',
-                                    marginTop: '5px',
-                                    overflow: 'auto',
-                                }}
-                                    variant="h4"
-                                    component="div"
-                                >{totalTime}</Typography>
+                        <div className="recipe-grid__card--time-and-options">
+                            <div onClick={() => handleClick(props.recipe.id)} className="recipe-grid__card--time-container">
+                                <Typography className="recipe-grid__card--time-detail" variant="h4">{totalTime}</Typography>
                             </div>
                             <CardActions>
                                 {document.title === 'Cooked Recipes' || document.title === 'Recently Viewed Recipes' ?
-                                    <BookmarkIcon /> : <MoreHorizIcon className='icon--black header__button'
-                                        onClick={(event) => { handlePopover(event); setEditedRecipeId(props.recipe.id) }} />}
-                                <Popover
-                                    id={popoverID}
-                                    open={open}
-                                    anchorEl={anchorEl}
-                                    onClose={handleClose}
+                                    <BookmarkIcon /> : <MoreHorizIcon className='icon--black header__button' onClick={(event) => { handlePopover(event); setEditedRecipeId(props.recipe.id) }} />}
+                                <Popover id={popoverID} open={open} anchorEl={anchorEl} onClose={handleClose}
                                     anchorOrigin={{
                                         vertical: 'bottom',
                                         horizontal: 'left',
-                                    }}
-                                >
-                                    <ul className={`dropdown`}>
-                                        <div className="dropdownButton">
+                                    }}>
+                                    <ul className="recipe-grid__popover">
+                                        <div className="recipe-grid__popover-button">
                                             <button onClick={handleFolderPopover}>Add to folder</button>
                                             <Popover
                                                 open={openFolder}
@@ -251,42 +188,29 @@ function RecipeCard(props) {
                                                     horizontal: 'right',
                                                 }}>
                                                 {recipeLists.map((folder, i) => (
-                                                    <div className="dropdownButton"  key={props.recipe.id}><button onClick={() => addRecipeToFolder(folder.id)} key={i}>{folder.list_name}</button></div>
+                                                    <div className="recipe-grid__popover-button" key={props.recipe.id}><button onClick={() => addRecipeToFolder(folder.id)} key={i}>{folder.list_name}</button></div>
                                                 ))}
 
                                             </Popover>
                                         </div>
-                                        <div className="div__dropdownButton">
-                                            <button className="dropdownButton" onClick={() => removeRecipe()}>Unsave from Recipe Box</button>
+                                        <div className="recipe-grid__popover-button-container">
+                                            <button className="recipe-grid__popover-button" onClick={() => removeRecipe()}>Unsave from Recipe Box</button>
                                             {document.title.includes('Your Recipe Box') ?
-                                                <button className="dropdownButton" onClick={() => removeRecipeFromUserFolder()} >Remove from this folder</button> : null}
+                                                <button className="recipe-grid__popover-button" onClick={() => removeRecipeFromUserFolder()} >Remove from this folder</button> : null}
                                         </div>
                                     </ul>
                                 </Popover>
                                 <Snackbar open={confirmFolder} autoHideDuration={1500} onClose={handleClose}>
                                     <Alert onClose={handleClose} severity="success" variant="filled"
-                                        icon={<CheckCircleOutlineIcon style={{ fill: 'white' }} />}
-                                        action={
-                                            <IconButton
-                                                size="small"
-                                                aria-label="close"
-                                                color="inherit"
-                                                onClick={handleClose}
-                                            >
-                                                <CloseIcon style={{ fill: 'white' }} />
-                                            </IconButton>
-                                        }
-                                    >
-                                        Recipe Added!
-                                    </Alert>
-                                </Snackbar>
-                            </CardActions>
+                                        icon={<CheckCircleOutlineIcon className="icon-white" />}
+                                        action={<IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                                            <CloseIcon className="icon-white" />
+                                        </IconButton>}>Recipe Added!</Alert></Snackbar></CardActions>
                         </div>
                     </div>
                 </Card>
-            </FadeIn >
+            </FadeIn>
         )
     }
 }
-
 export default RecipeCard;
