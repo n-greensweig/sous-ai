d211bf1345f35ec1e770c75bd375c904fd587ae5
Noah Greensweig <ngreensweig@gmail.com>
2024-08-13 13:48:41 -0500

    Moved styling into separate css file from RecipeGrid.jsx.

--- src/components/RecipeItems/RecipeGrid/RecipeGrid.jsx
+++ src/components/RecipeItems/RecipeGrid/RecipeGrid.jsx
@@ -1,23 +1,13 @@
 import { Grid } from "@mui/material";
 import RecipeGridCards from './RecipeGridCards/RecipeGridCards';
 import RecipeGridSubheading from './RecipeGridSubheading/RecipeGridSubheading';
-
+import './RecipeGrid.css';
 function RecipeGrid({ recipes, listName, numOfRecipes, searchQuery, setSearchQuery, isXsScreen, isSmScreen, id }) {
     return (
-        <Grid container spacing={2} minHeight={'5vh'} className="full-width-background"
-            style={{
-                marginTop: '0px', margin: '0 auto',
-                padding: '20px 10px', backgroundColor: '#FFF', flexGrow: 1,
-                width: isXsScreen || isSmScreen ? '100vw' : null, justifyContent: isXsScreen || isSmScreen ? 
-                    numOfRecipes === 0 ? 'left' : 'center' : 'left', paddingLeft: 
-                    (isXsScreen || isSmScreen) && numOfRecipes === 0 ? '10%' : null,
-            }}
-        >
-            <div className='div__recipeGrid--container' style={{
-                display: 'flex', flexDirection: 'column',
-                paddingLeft: isXsScreen || isSmScreen ? null : '10px',
-            }}>
-                <div style={{ marginLeft: '0px', }}>
+        <Grid container spacing={2}
+            className={`recipe-grid__container ${numOfRecipes === 0 ? 'padding-left-ten' : null} ${numOfRecipes !== 0 ? 'recipe-grid__container--center' : ''}`}>
+            <div className='recipe-grid__subcontainer'>
+                <div className='recipe-grid__subheading-wrapper'>
                     {listName && (
                         <RecipeGridSubheading listName={listName} numOfRecipes={numOfRecipes} searchQuery={searchQuery} setSearchQuery={setSearchQuery} id={id}
                             isXsScreen={isXsScreen} isSmScreen={isSmScreen} />
