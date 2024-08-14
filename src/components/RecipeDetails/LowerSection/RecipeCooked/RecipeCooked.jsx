e72964e0b715df368e4ddea9c87467af4f9b6cb4
Noah Greensweig <ngreensweig@gmail.com>
2024-08-14 16:13:56 -0500

    Removed inline styles from RecipeCooked component.

--- src/components/RecipeDetails/LowerSection/RecipeCooked/RecipeCooked.jsx
+++ src/components/RecipeDetails/LowerSection/RecipeCooked/RecipeCooked.jsx
@@ -1,6 +1,7 @@
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-function RecipeCooked({ isCooked, setIsCooked, id, dispatch}) {
+import './RecipeCooked.css';
+function RecipeCooked({ isCooked, setIsCooked, id, dispatch }) {
     // Function to toggle the cooked state of the recipe
     const toggleCooked = (e, cooked) => {
         e.preventDefault();
@@ -11,19 +12,14 @@ function RecipeCooked({ isCooked, setIsCooked, id, dispatch}) {
     return (
         <div>
             <strong>Have you cooked this?</strong>
-            <p style={{ cursor: 'pointer', marginBottom: '10%' }}>
-                {!isCooked ? (
-                    <span onClick={e => toggleCooked(e, true)} style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
-                        <CheckCircleOutlineIcon style={{ fontSize: '200%', fill: 'black' }} /> Mark as cooked
-                    </span>
-                ) : (
-                    <span onClick={e => toggleCooked(e, false)} style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
-                        <CheckCircleIcon style={{ fontSize: '200%', fill: 'black', }} /> Yes I have
-                    </span>
-                )}
+            <p className='recipe-details__p--cooked pointer'>
+                {!isCooked ? (<span className='display-flex align-center' onClick={e => toggleCooked(e, true)}>
+                    <CheckCircleOutlineIcon className='fill-black' /> Mark as cooked
+                </span>) : (<span onClick={e => toggleCooked(e, false)}>
+                    <CheckCircleIcon className='fill-black' /> Yes I have
+                </span>)}
             </p>
         </div>
     )
 }
-
 export default RecipeCooked;
