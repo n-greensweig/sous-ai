609f3395dd3b31aafdf209fd92840d0db93ecd1c
Noah Greensweig <ngreensweig@gmail.com>
2024-08-14 09:23:05 -0500

    Removed inline styles from AutoScroll.jsx.

--- src/components/AutoScroll/AutoScroll.jsx
+++ src/components/AutoScroll/AutoScroll.jsx
@@ -3,17 +3,17 @@ function AutoScroll({ type }) {
     return (
         type === 'scroll-up' ?
             // If type is 'scroll-up', then render ability scroll to top of the page
-            <p style={{ marginTop: '3%' }} className="auto-scroll__p" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                <span className="auto-scroll__span" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', textDecoration: 'underline', }}>
+            <p className="auto-scroll__p recipe-details__auto-scroll-one" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+                <span className="auto-scroll__span">
                     <span>Back to recipe</span>
-                    <FaTurnUp className="icon-black auto-scroll__icon" style={{ marginLeft: '3px', fill: 'black', }} />
+                    <FaTurnUp className="icon-black auto-scroll__icon" />
                 </span>
             </p> :
             // If type is 'scroll-down', then render ability scroll to bottom of the page
-            <p style={{ marginBottom: '15%', textDecoration: 'underline', }} className="auto-scroll__p" onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })}>
+            <p className="auto-scroll__p recipe-details__auto-scroll-two" onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })}>
                 <span className="auto-scroll__span">
                     Read recipe notes
-                    <FaArrowTurnDown className="icon-black auto-scroll__icon" style={{ marginLeft: '3px', fill: 'black', }} />
+                    <FaArrowTurnDown className="icon-black auto-scroll__icon" />
                 </span>
             </p>
     );
