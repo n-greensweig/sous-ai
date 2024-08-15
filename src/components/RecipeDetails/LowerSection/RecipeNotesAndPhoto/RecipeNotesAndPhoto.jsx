9f7135c4ea6d8c30091fde4481793dd88e7a954a
Noah Greensweig <ngreensweig@gmail.com>
2024-08-15 09:34:55 -0500

    Fixed bug.

--- src/components/RecipeDetails/LowerSection/RecipeNotesAndPhoto/RecipeNotesAndPhoto.jsx
+++ src/components/RecipeDetails/LowerSection/RecipeNotesAndPhoto/RecipeNotesAndPhoto.jsx
@@ -3,7 +3,7 @@ import RecipeNotes from "./RecipeNotes/RecipeNotes";
 import './RecipeNotesAndPhoto.css';
 function RecipeNotesAndPhoto({ imageList, comments, dispatch, id }) {
     return (
-        <div id='notes-photo-container display-flex align-fs justify-sb width-100'>
+        <div className='notes-photo-container display-flex align-fs justify-sb width-100'>
             <RecipePhotos imageList={imageList} />
             <RecipeNotes comments={comments} dispatch={dispatch} id={id} />
         </div>
