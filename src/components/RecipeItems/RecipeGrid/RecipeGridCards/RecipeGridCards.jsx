dbcdcb4feb07e6194824d769d62153138384e7cf
Noah Greensweig <ngreensweig@gmail.com>
2024-09-02 22:19:57 -0500

    Got mobile subheading working as desired.

--- src/components/RecipeItems/RecipeGrid/RecipeGridCards/RecipeGridCards.jsx
+++ src/components/RecipeItems/RecipeGrid/RecipeGridCards/RecipeGridCards.jsx
@@ -2,7 +2,10 @@ import RecipeCard from '../RecipeCard/RecipeCard';
 import './RecipeGridCards.css';
 function RecipeGridCards({ recipes, listName, path }) {
     return (
-        <div className={`recipe-grid-container ${path !== '/recipe-box' ? 'grid' : 'flex'}`}>
+        <div className={`recipe-grid-container ${path !== '/recipe-box' ? 'grid' : 'flex'}
+        ${path === '/recipe-box' || path === '/recipe-box/cooked' || path === '/recipe-box/recent' ?
+             'mt-175' : 'mt-225'}
+        `}>
             {recipes.map((recipe, index) => (
                 <div className='recipe-card' key={index}>
                     <RecipeCard key={recipe.id} recipe={recipe} index={index} listName={listName} />
