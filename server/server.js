34a321b4102c97b0eb7a045ea3c7f409577471fb
Noah Greensweig <ngreensweig@gmail.com>
2024-09-11 08:25:21 -0500

    Preloaded fonts and images and set up cache policy in server.js

--- server/server.js
+++ server/server.js
@@ -23,19 +23,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Passport Session Configuration //
 app.use(sessionMiddleware);
 
-// start up passport sessions
+// Start up passport sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
-/* Routes */
+// Set cache control headers for fonts
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'public, max-age=31536000'); // Cache for 1 year
+  next();
+});
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Routes
 app.use('/api/user', userRouter);
 app.use('/api/recipe', recipeRouter);
 app.use('/api/completions', openaiRouter);
 app.use('/api/photos', imageRouter);
 
-// Serve static files
-app.use(express.static('build'));
-app.use((req, res) => {
+// Catch-all route for serving index.html
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
